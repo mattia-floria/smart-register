7a4c1028066fc7907e0fabd50cd6485619d2a2c9
matti <mattia.floria10@gmail.com>
2026-05-08 17:06:11 +0200

    Aggiornamento versione a 1.0.1 e ottimizzazioni APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,19 +12,23 @@ android {
         applicationId = "com.afloria.smartregister"
         minSdk = 33
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1"
+        versionCode = 3
+        versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+            ndk {
+                abiFilters.add("arm64-v8a")
+            }
         }
     }
     compileOptions {
